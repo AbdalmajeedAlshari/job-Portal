0f18244fba0dadbc5a2308548a4558cfc6b29820
sumer <sumer5020@gmail.com>
2017-11-29 19:11:49 +0300

    empty Project

---
+++ jobPortal/jobPortal/Controllers/IndixController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jobPortal.Controllers
+{
+    public class IndixController : Controller
+    {
+        // GET: Indix
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
