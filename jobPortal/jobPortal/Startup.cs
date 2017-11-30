cfa7be8a4b2d504a23ba130e343102eebd06e669
sumer <sumer5020@gmail.com>
2017-11-30 23:33:06 +0300

    New Full MVC Project

---
+++ jobPortal/jobPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jobPortal.Startup))]
+namespace jobPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
